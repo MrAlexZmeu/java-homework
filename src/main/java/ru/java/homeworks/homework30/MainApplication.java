394ffe7a6e25d6fd45cce5e78c40875c81f8eaf1
MrAlexZmeu <mralekseizmeu@gmail.com>
2024-02-15 06:28:26 +0300

    hw30

---
+++ src/main/java/ru/java/homeworks/homework30/MainApplication.java
@@ -0,0 +1,53 @@
+package ru.java.homeworks.homework30;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 6, 7, 8, 4, 1, 2, 4};
+        System.out.println(Arrays.toString(arrayAfterLastOne(arr)));
+        int[] arrOne = {1,1};
+        System.out.println(arrayConsistOfOneTwo(arrOne));
+    }
+
+    public static int[] arrayAfterLastOne(int[] arr) {
+        int n = -1;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] == 1) {
+                n = i;
+                break;
+            }
+        }
+
+        if (n == -1) {
+            throw new RuntimeException();
+        }
+
+        int[] result = new int[arr.length - n - 1];
+        for (int i = n + 1; i < arr.length; i++) {
+            result[i - n - 1] = arr[i];
+        }
+        return result;
+    }
+
+    public static boolean arrayConsistOfOneTwo(int[] arr) {
+        int countOne = 0;
+        int countTwo = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 2 && arr[i] < 1) {
+                return false;
+            }
+            if (arr[i] == 1) {
+                countOne++;
+            }
+            if (arr[i] == 2) {
+                countTwo++;
+            }
+        }
+        if (countOne == 0 || countTwo == 0) {
+            return false;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/ru/java/homeworks/homework30/test/MainApplicationTest.java
@@ -0,0 +1,37 @@
+package ru.java.homeworks.homework30.test;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import ru.java.homeworks.homework30.MainApplication;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainApplicationTest {
+
+    @Test
+    public void testArrayAfterLastOne(){
+        int[] test = {1,2,1,2,2};
+        int[] res = {2,2};
+        Assertions.assertArrayEquals(res, MainApplication.arrayAfterLastOne(test));
+
+        int[] test1 = {2,2,2,2};
+        boolean exceptionThrown = false;
+
+        try {
+            MainApplication.arrayAfterLastOne(test1);
+        } catch (RuntimeException e) {
+            exceptionThrown = true;
+        }
+
+        assertTrue(exceptionThrown);
+    }
+
+
+    @Test
+    public void testArrayConsistOfOneTwo(){
+        int[] test = {1,2};
+        assertTrue(MainApplication.arrayConsistOfOneTwo(test));
+    }
+
+}
